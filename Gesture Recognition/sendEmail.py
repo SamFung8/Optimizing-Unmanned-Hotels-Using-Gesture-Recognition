509f4bd052705dd3622652d5aeb186a0aff49197
SamFung8 <93874864+SamFung8@users.noreply.github.com>
2023-06-12 23:42:35 +0800

    code change

--- Gesture Recognition/sendEmail.py
+++ Gesture Recognition/sendEmail.py
@@ -9,7 +9,8 @@ def send():
     content["subject"] = "City Hotel Booking"  # 郵件標題
     content["from"] = "fyptesting2023@gmail.com"  # 寄件者
     content["to"] = "fungkingsh2@gmail.com"  # 收件者
-    content.attach(MIMEText("Demo python send email"))  # 郵件純文字內容
+    content.attach(MIMEText("Thank you for your reservation! \n\n"
+                            "A QR code is attached to this email. Please use this QR code to check in when using a public machine."))  # 郵件純文字內容
     content.attach(MIMEImage(Path(r"C:\xampp\htdocs\project_new\booking_record.png").read_bytes()))  # 郵件圖片內容
 
     with smtplib.SMTP(host="smtp.gmail.com", port="587") as smtp:  # 設定SMTP伺服器
