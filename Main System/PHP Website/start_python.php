b116797c7648ee4ac7acf61f96ab21cd491199c2
SamFung8 <93874864+SamFung8@users.noreply.github.com>
2023-07-17 01:49:25 +0800

    final version

---
+++ Main System/PHP Website/start_python.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title></title>
+  <style>
+    .button {
+      background-color: #4CAF50;
+      border: none;
+      color: white;
+      padding: 15px 32px;
+      text-align: center;
+      text-decoration: none;
+      display: inline-block;
+      font-size: 16px;
+      margin: 4px 2px;
+      cursor: pointer;
+    }
+
+  </style>
+</head>
+
+<body>
+
+  <h1>Guideline Of Using Public Machine</h1>
+  
+  <h3>Mouse Mode</h3>
+  <table width="100%">
+    <tr>
+      <td><img src="img/right-click.png" width="70" height="70"></td>
+      <td><img src="img/mouse_moving.png" width="80" height="100"></td>
+      <td><img src="img/change_mode.png" width="80" height="100"></td>
+    </tr>
+    <tr>
+      <td>Right-Click</td>
+      <td>Mouse Moving</td>
+      <td>Change to keyboard mode</td>
+    </tr>
+  </table>
+  <br>
+  
+  <h3>keyboard Mode</h3>
+  <table width="100%">
+    <tr>
+      <td><img src="img/mouse_moving.png" width="80" height="100"></td>
+      <td><img src="img/change_mode.png" width="80" height="100"></td>
+    </tr>
+    <tr>
+      <td>Select input text</td>
+      <td>Change to mouse mode</td>
+    </tr>
+  </table>
+  <h3>Each atchion please hold it about 2 second!</h3>
+
+  <a href="./index_python.php" class="button">Start</a>
+
+</body>
+
+</html>
